4c4d40b9b28832c1123540df19515b57e4b2fa48
Jakub Kret <jakub.a.kret@gmail.com>
2019-09-14 23:18:13 +0200

    Add Result Screen

---
+++ screens/ResultScreen/ResultScreen.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  Button,
+  Card,
+  List,
+  Text,
+} from 'react-native-paper';
+
+import {
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
+
+export default function ResultScreen({ onClose, results }) {
+  return (
+    <Card
+      style={styles.questionList}
+    >
+      <Card.Title
+        title="Results"
+      />
+      <ScrollView
+        style={styles.list}
+      >
+        <Card.Content>
+          <List.Section>
+            {results.map((result) => (
+              <Card
+                key={result.question}
+                style={styles.questionCard}
+              >
+                <Card.Title
+                  title={result.question}
+                />
+                <Card.Content>
+                  <Text>
+                    {result.answer}
+                  </Text>
+                </Card.Content>
+              </Card>
+            ))}
+          </List.Section>
+        </Card.Content>
+      </ScrollView>
+      <Button
+        mode="outlined"
+        onPress={onClose}
+      >
+        Close
+      </Button>
+    </Card>
+  );
+}
+
+ResultScreen.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  results: PropTypes.arrayOf(PropTypes.shape({
+    question: PropTypes.string,
+    answer: PropTypes.string,
+  })).isRequired,
+};
+ResultScreen.defaultProps = {};
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+  },
+  questionCard: {
+    marginTop: 4,
+    marginBottom: 4,
+  },
+  questionList: {
+    flex: 1,
+    marginTop: 24,
+  }
+});
